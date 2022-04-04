dfc4c6262f6b6b14ec23ad0ee3a1aaa670b3fec9
Nikolay Stankov <nstankov@Nikolays-MBP.wlan-01-normal>
2022-04-03 23:47:37 -0400

    fast commit

--- random-output.js
+++ random-output.js
@@ -1,32 +1,69 @@
 module.exports = function (RED) {
-  function electOutput(i, context){
-    context.set('CurrentOutput', i);
-    context.set('CurrentOutputElectionTimestamp', Date.now());
+  function electNode(context, node, outputNum) {
+    //Check if outputNum is eligible for re-election
+    if (checkReelectionEligibility(context, outputNum, node) == true) {
+      context.set("lastElectedNode", outputNum);
+      context.set("lastElectedTime", Date.now());
+      //2m minutes to expire
+      context.set("lastElectedTimeNode" + electedNode, Date.now() + 120000);
+      node.log(
+        "node-red-contrib: Elected node " +
+          electedNode +
+          " at " +
+          context.get("lastElectedTime")
+      );
+    } else {
+      return false;
+    }
+  }
+  function checkReelectionEligibility(context, outputNum, node) {
+    context.get("lastElectedTimeNode" + outputNum);
+    //check if the node is eligible for re-election
+    if (context.get("lastElectedTimeNode" + outputNum) > Date.now()) {
+      node.log(
+        "node-red-contrib: Node " +
+          outputNum +
+          " is not eligible for re-election"
+      );
+      electNode(context, node, outputNum++);
+      return false;
+    } else {
+      node.log(
+        "node-red-contrib: Node " + outputNum + " is eligible for re-election"
+      );
+      return true;
+    }
   }
 
   function RandomOutputNode(config) {
     RED.nodes.createNode(this, config);
     let node = this;
     let context = this.context();
+
+    let electedNode = context.get("lastElectedNode");
+    let electedTime = context.get("lastElectedTime");
+
     const numberOfOutputs = config.outputs;
 
     node.on("input", function (msg) {
       let output = new Array(numberOfOutputs);
-      node.log(
-        "Generating " + numberOfOutputs + " random outputs for node " + node.id
-      );
-      //Get a single random number from output range
-      for (let i = 0; i < numberOfOutputs; i++) {
-        output[i] = Math.floor(Math.random() * numberOfOutputs);
+      let chosen;
+      if (electedNode !== "" && electedNode !== undefined) {
+        chosen = electedNode;
+        if (electedTime > Date.now() - 30000) {
+          chosen = electedNode;
+          output[chosen] = msg;
+          node.send(output);
+        } else {
+          for (let outputNum = 0; outputNum < numberOfOutputs; outputNum++) {
+            chosen = outputNum;
+            electNode(context, node, outputNum);
+          }
+          chosen = electedNode;
+          output[chosen] = msg;
+          node.send(output);
+        }
       }
-      //Sort the output array
-      output.sort();
-      //Set the output
-      electOutput(output[0], context);
-      node.log("Output: " + output[0]);
-      
-      output[context.get("CurrentOutput")] = msg;
-      node.send(output);
     });
   }
 
