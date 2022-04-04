a8c782679d99c07378bcd1a8083ae9fe55e91b42
Nikolay Stankov <nstankov@Nikolays-MBP.wlan-01-normal>
2022-04-03 23:35:36 -0400

    fast commit

--- random-output.js
+++ random-output.js
@@ -1,8 +1,13 @@
 module.exports = function (RED) {
+  function electOutput(i, context){
+    context.set('CurrentOutput', i);
+    context.set('CurrentOutputElectionTimestamp', Date.now());
+  }
+
   function RandomOutputNode(config) {
     RED.nodes.createNode(this, config);
     let node = this;
-
+    let context = this.context();
     const numberOfOutputs = config.outputs;
 
     node.on("input", function (msg) {
@@ -10,11 +15,18 @@ module.exports = function (RED) {
       node.log(
         "Generating " + numberOfOutputs + " random outputs for node " + node.id
       );
-      for (let outputNum = 0; outputNum < numberOfOutputs; outputNum++) {
-        chosen = outputNum;
-        output[chosen] = msg;
-        node.send(output);
+      //Get a single random number from output range
+      for (let i = 0; i < numberOfOutputs; i++) {
+        output[i] = Math.floor(Math.random() * numberOfOutputs);
       }
+      //Sort the output array
+      output.sort();
+      //Set the output
+      electOutput(output[0], context);
+      node.log("Output: " + output[0]);
+      
+      output[context.get("CurrentOutput")] = msg;
+      node.send(output);
     });
   }
 
