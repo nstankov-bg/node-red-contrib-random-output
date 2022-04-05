69762f613593acb0e6ddf9813e315d62fe772dd7
Nikolay Stankov <nstankov@Nikolays-MacBook-Pro.local>
2022-04-05 13:40:18 -0400

    fast commit

--- random-output.js
+++ random-output.js
@@ -95,7 +95,7 @@ module.exports = function (RED) {
           node.log("node-red-contrib: Election has expired");
           if (context.get("lastElectedNode") + 1 === numberOfOutputs) {
             restartOutputNode = 0;
-          } elseif (context.get("lastElectedNode") + 1 > numberOfOutputs) {
+          } else if (context.get("lastElectedNode") + 1 > numberOfOutputs) {
             restartOutputNode = 0;
           } else {
             restartOutputNode = context.get("lastElectedNode") + 1;
