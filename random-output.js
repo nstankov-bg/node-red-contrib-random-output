1d58a366373a7b407f0bd1ada4ff09c897c9fc63
Nikolay Stankov <27363885+nstankov-bg@users.noreply.github.com>
2023-03-28 15:00:23 -0400

    Update random-output.js

--- random-output.js
+++ random-output.js
@@ -1,127 +1,52 @@
 module.exports = function (RED) {
   function electNode(context, node, outputNum, ReElectionBan) {
-    node.log(
-      "node-red-contrib: Checking if output " +
-        outputNum +
-        " is eligible for re-election"
-    );
-    if (
-      checkReelectionEligibility(context, outputNum, node, ReElectionBan) ==
-      true
-    ) {
+    if (checkReelectionEligibility(context, outputNum)) {
       context.set("lastElectedNode", outputNum);
       context.set("lastElectedTime" + outputNum, Date.now());
-      context.set(
-        "ElectionBannedUntill" + outputNum,
-        Date.now() + ReElectionBan
-      );
-      node.log(
-        "node-red-contrib: Node " + outputNum + " is eligible for re-election"
-      );
-      node.log(
-        "node-red-contrib: Elected node " +
-          context.get("lastElectedNode") +
-          " at " +
-          context.get("lastElectedTime" + outputNum)
-      );
+      context.set("ElectionBannedUntill" + outputNum, Date.now() + ReElectionBan);
+      node.log(`node-red-contrib: Node ${outputNum} is eligible for re-election`);
       return true;
     } else {
-      node.log(
-        "node-red-contrib: Node " +
-          outputNum +
-          " will be eligable at: " +
-          context.get("ElectionBannedUntill" + outputNum)
-      );
-      node.log(
-        "node-red-contrib: " + outputNum + " is not eligible for re-election"
-      );
+      node.log(`node-red-contrib: Node ${outputNum} will be eligible at: ${context.get("ElectionBannedUntill" + outputNum)}`);
+      node.log(`node-red-contrib: ${outputNum} is not eligible for re-election`);
       return false;
     }
   }
 
   function checkReelectionEligibility(context, outputNum) {
-    if (context.get("ElectionBannedUntill" + outputNum) > Date.now()) {
-      return false;
-    } else {
-      return true;
-    }
+    return context.get("ElectionBannedUntill" + outputNum) <= Date.now();
   }
 
   function RandomOutputNode(config) {
     RED.nodes.createNode(this, config);
-    let node = this;
-    let context = this.context();
+    const node = this;
+    const context = this.context();
 
     const ReElectionBan = 4 * 60 * 60 * 1000; // 4 hours
     const ElectionTime = 2500; // 2.5 seconds
-
     const numberOfOutputs = config.outputs - 1;
 
     node.on("input", function (msg) {
-      node.log("------------------------------------------------------");
-      node.log("node-red-contrib: Random Output Node created");
-      node.log("node-red-contrib: Number of outputs: " + (numberOfOutputs + 1));
-
-      if (context.get("lastElectedNode") !== undefined) {
-        node.log(
-          "node-red-contrib: Currently elected node: " +
-            context.get("lastElectedNode")
-        );
-        node.log(
-          "node-red-contrib: Last elected time: " +
-            context.get("lastElectedTime" + context.get("lastElectedNode"))
-        );
-        node.log("node-red-contrib: Current Unix Time: " + Date.now());
-      } else {
-        node.log("node-red-contrib: No node has been elected yet");
-      }
-      node.log("------------------------------------------------------");
-
-      let output = new Array(numberOfOutputs);
+      const lastElectedNode = context.get("lastElectedNode");
+      let output = Array(numberOfOutputs).fill(null);
       let chosen;
-      let restartOutputNode;
-      let nextRestartNode;
 
-      if (
-        context.get("lastElectedNode") !== "" &&
-        context.get("lastElectedNode") !== undefined
-      ) {
-        chosen = context.get("lastElectedNode");
-        if (
-          context.get("lastElectedTime" + chosen) > Date.now() - ElectionTime &&
-          context.get("lastElectedTime" + chosen) !== undefined
-        ) {
-          chosen = context.get("lastElectedNode");
+      if (lastElectedNode !== undefined && lastElectedNode !== "") {
+        chosen = lastElectedNode;
+        if (context.get("lastElectedTime" + chosen) > Date.now() - ElectionTime) {
           output[chosen] = msg;
           node.send(output);
         } else {
-          node.log("node-red-contrib: Election has expired");
-          if (context.get("lastElectedNode") + 1 > numberOfOutputs) {
-            restartOutputNode = 0;
-          } else {
-            restartOutputNode = context.get("lastElectedNode") + 1;
-          }
-          if (
-            electNode(context, node, restartOutputNode, ReElectionBan) == true
-          ) {
-            chosen = context.get("lastElectedNode");
-            output[chosen] = msg;
-            node.send(output);
-          } else {
-            nextRestartNode = restartOutputNode + 1;
-            electNode(context, node, nextRestartNode, ReElectionBan);
-          }
+          const nextNode = (chosen + 1) % (numberOfOutputs + 1);
+          electNode(context, node, nextNode, ReElectionBan) && node.send(output);
         }
       } else {
         for (let outputNum = 0; outputNum <= numberOfOutputs; outputNum++) {
-          if (electNode(context, node, outputNum, ReElectionBan) == true) {
-            node.log("node-red-contrib: Elected node " + outputNum);
+          if (electNode(context, node, outputNum, ReElectionBan)) {
             chosen = context.get("lastElectedNode");
             output[chosen] = msg;
             node.send(output);
             break;
-          } else {
-            node.log("node-red-contrib: Node " + outputNum + " is banned");
           }
         }
       }
